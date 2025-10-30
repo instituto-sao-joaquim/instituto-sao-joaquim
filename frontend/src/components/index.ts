26112593a5117e809710ea1bc89e22457b3c6f1f
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-10-30 11:21:34 -0300

    chore: add index.ts to export all components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,9 @@
+export { default as Button } from './common/Button';
+export { default as CardAbout } from './common/CardAbout';
+export { default as CardHero } from './common/CardHero';
+export { default as Badge } from './common/SectionBadge';
+export { default as Hero } from './Hero';
+export { default as Header } from './layout/Header';
+export { default as About } from './sections/About';
+export { default as Projects } from './sections/Projects';
+
