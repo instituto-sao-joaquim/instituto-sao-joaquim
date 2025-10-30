ef557875c400b5cc86edb68e7488a8ddfc44f239
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-10-30 11:23:03 -0300

    feat: update global styles and import main components in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
+import { About, Header, Hero, Projects } from "@/components";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black" id="home">
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
     </div>
   );
 }
