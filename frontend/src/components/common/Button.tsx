6cb617545936f884aca87e84f5a3cddd23adb683
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-11-03 08:11:01 -0300

    style(ui): improve design and responsiveness of common button and cardHero components

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -3,20 +3,44 @@ import React from 'react';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   bgColor?: string;
   textColor?: string;
+  fontSize?: string;
+  scrollToId?: string;
   children: React.ReactNode;
 }
 
 export default function Button({
   bgColor = 'bg-green-400',
   textColor = 'text-white',
+  fontSize = 'text-sm',
+  scrollToId,
   children,
   className = '',
   ...props
 }: ButtonProps) {
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (scrollToId) {
+      const element = document.getElementById(scrollToId);
+      if (element) {
+        const headerOffset = 70;
+        const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth',
+        });
+      }
+    }
+
+    if (props.onClick) props.onClick(e);
+  };
+
   return (
     <button
-      className={`px-4 py-1.5 rounded-xl transition hover:brightness-90 ${bgColor} ${textColor} ${className}`}
+      className={`px-4 py-1.5 rounded-xl transition hover:brightness-90 ${bgColor} ${textColor} ${className} ${fontSize} cursor-pointer`}
       {...props}
+      onClick={handleClick}
     >
       {children}
     </button>
--- frontend/src/components/common/CardHero.tsx
+++ frontend/src/components/common/CardHero.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 
 export default function CardHero({ icon, title, value, stroke, fill }: CardProps) {
   return (
-    <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-lg p-4 flex flex-col items-center text-center shadow-md w-60">
+    <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-lg p-4 flex flex-col items-center text-center shadow-md md:w-60">
       <div className={`mb-3 ${stroke} ${fill}`}>{icon}</div>
       <h3 className="text-2xl font-bold mb-2">{value}</h3>
       <p className="text-neutral-300 text-sm">{title}</p>
