f7b2a61431e74fbbad21e03119be0436923c3057
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-12-13 12:21:02 -0300

    fix: update next version and adjust next.config.ts settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: path.join(__dirname),
+  allowedDevOrigins: ['192.168.3.6', 'localhost'],
 };
 
 export default nextConfig;
