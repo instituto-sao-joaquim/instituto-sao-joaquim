cdd66914a94a0a3200e4ca744f0dd8ecbc9ff59e
Ana Ferreira <anapaulaferreira0309@gmail.com>
2025-12-27 09:35:41 -0300

    feat: add Google Analytics GA4 tracking

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Footer, Header } from "@/components";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,6 +27,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1FLGJGJCTN"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-1FLGJGJCTN');
+          `}
+        </Script>
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
